a4043dcaa0cf01e5fdf6f89992ac458476138247
therealeddy <eduardo.mfonseca@hotmail.com>
2019-12-09 22:12:33 -0300

    aula 4 - migration e model de agendamento

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,11 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -11,6 +15,15 @@ routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
+
 routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
+
+routes.post(
+  '/files',
+  multer(multerConfig).single('file'),
+  FileController.store
+);
 
 export default routes;

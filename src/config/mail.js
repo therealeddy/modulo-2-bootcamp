38e6087afa7ddf9807688dfaa196747ffd5ab4d9
therealeddy <eduardo.mfonseca@hotmail.com>
2019-12-10 23:27:08 -0300

    aula 17 - Filas

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '13943acffcbc9d',
+    pass: '2bbe10667f1471',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
